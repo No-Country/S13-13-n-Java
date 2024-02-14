71aad9cdad91375a483d075db2860ba421c6d4b7
Matias <akucfh@gmail.com>
2024-02-14 02:32:02 +0100

    first commit

---
+++ Back/src/test/java/com/s13nocoutry/LearnConnect/LearnConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s13nocoutry.LearnConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
