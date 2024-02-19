a46a385817b31c187123ca1b3aa2f50722b78b22
geraldineac <geraldineacevedo29@gmail.com>
2024-02-19 00:51:14 -0300

    new branch

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -7,7 +7,17 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isLoading = true;
+
+
   constructor(private router: Router) {}
+
+  // Simula la carga de datos (por ejemplo, después de una solicitud HTTP)
+  ngOnInit() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000); // Simula una carga de 2 segundos
+  }
 
   hasRoute(route:string){
     return this.router.url.includes(route);
--- Front/src/app/layout/chat-room/chat-room.component.ts
+++ Front/src/app/layout/chat-room/chat-room.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ChatMessage } from 'src/app/interfaces/chat-message';
+import { FormBuilder, FormGroup } from '@angular/forms';
 //import { ChatService } from '../../services/chat.service';
 
 @Component({
@@ -7,19 +9,38 @@ import { Observable } from 'rxjs';
   templateUrl: './chat-room.component.html',
   styleUrls: ['./chat-room.component.css']
 })
+
 export class ChatRoomComponent implements OnInit{
   isLoading: boolean = true;
   chatList: any;
 
-  constructor(/* private chatService: ChatService */) {
+  searchForm: any = FormGroup;
+searchCriteria: any = '';
 
-   }
+
+
+  constructor(/* private chatService: ChatService */ private fb: FormBuilder) {
+      this.searchForm = this.fb.group({
+        searchCriteria: [''], // Inicializa el campo de búsqueda
+      });
+    }
+
 
   // Simula una operación asíncrona, como cargar datos de un servidor
   ngOnInit() {
     setTimeout(() => {
       this.isLoading = false;
     }, 2000);
+
+    this.searchForm.get('searchCriteria').valueChanges.subscribe(() => {
+      this.updateSearch();
+    });
   }
 
-}
+  updateSearch() {
+    const criteria = this.searchForm.get('searchCriteria').value;
+  
+  }
+  }
+
+
