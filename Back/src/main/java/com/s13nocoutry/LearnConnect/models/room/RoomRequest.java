d1fc8a704345a4947d3cafb5dd6a7cf2ffbbf512
sturniolo <br1.sturniolo@gmail.com>
2024-02-20 12:36:26 -0300

    [ADD]relacion entre Room y Chat, [FIX] estructura de carpetas

--- Back/src/main/java/com/s13nocoutry/LearnConnect/models/room/RoomRequest.java
+++ Back/src/main/java/com/s13nocoutry/LearnConnect/models/room/RoomRequest.java
@@ -2,7 +2,9 @@ package com.s13nocoutry.LearnConnect.models.room;
 
 import com.s13nocoutry.LearnConnect.models.Chat.Chat;
 import com.s13nocoutry.LearnConnect.models.user.User;
-import jakarta.persistence.*;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,18 +12,13 @@ import lombok.Setter;
 
 import java.util.List;
 
-@Entity
-@Getter
-@Setter
-@NoArgsConstructor
 @AllArgsConstructor
-public class Room {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+@NoArgsConstructor
+@Setter
+@Getter
+public class RoomRequest {
+    // ToDO: revisar el formato del dto
     private Long id;
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id")
     private Chat chat;
     private List<User> users;
     private Boolean block;
--- Back/src/main/java/com/s13nocoutry/LearnConnect/models/room/RoomResponse.java
+++ Back/src/main/java/com/s13nocoutry/LearnConnect/models/room/RoomResponse.java
@@ -2,7 +2,7 @@ package com.s13nocoutry.LearnConnect.models.room;
 
 import com.s13nocoutry.LearnConnect.models.Chat.Chat;
 import com.s13nocoutry.LearnConnect.models.user.User;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,13 +15,9 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Room {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class RoomResponse {
+    // ToDO: revisar el formato del dto
     private Long id;
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id")
     private Chat chat;
     private List<User> users;
     private Boolean block;
