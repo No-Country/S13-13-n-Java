47f806f2ffddd0967dbadb323bccf5c0ab90dca0
Belen Taborda <mbelen.taborda41@gmail.com>
2024-02-20 19:05:18 -0300

    agrego controller de message

---
+++ Back/src/main/java/com/s13nocoutry/LearnConnect/models/Message/MessageReponse.java
@@ -0,0 +1,20 @@
+package com.s13nocoutry.LearnConnect.models.Message;
+
+import com.s13nocoutry.LearnConnect.models.Chat.Chat;
+import com.s13nocoutry.LearnConnect.models.user.User;
+import lombok.*;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class MessageReponse {
+
+    private String content;
+    private User user;
+    private Date hour;
+    private Chat chat;
+}
