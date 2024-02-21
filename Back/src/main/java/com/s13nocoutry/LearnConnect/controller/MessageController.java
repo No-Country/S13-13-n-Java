e4b17b1b540e4a6b14c1e75f36bb104a943abc93
Belen Taborda <mbelen.taborda41@gmail.com>
2024-02-21 20:11:26 -0300

    modifico el controller de message

--- Back/src/main/java/com/s13nocoutry/LearnConnect/controller/MessageController.java
+++ Back/src/main/java/com/s13nocoutry/LearnConnect/controller/MessageController.java
@@ -1,25 +1,50 @@
 package com.s13nocoutry.LearnConnect.controller;
 
+;
+
 import com.s13nocoutry.LearnConnect.models.Message.Message;
+import com.s13nocoutry.LearnConnect.models.user.UserResponse;
 import com.s13nocoutry.LearnConnect.service.abstraction.MessageService;
-import com.s13nocoutry.LearnConnect.service.implementation.MessageServiceImp;
-import lombok.RequiredArgsConstructor;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
+import com.s13nocoutry.LearnConnect.service.abstraction.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequiredArgsConstructor
+
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/messages")
 public class MessageController {
+   @Autowired
+    private MessageService messageService;
+   private UserService userService;
 
-    @MessageMapping("/message")
-    @SendTo("/topic/message")
+   @PostMapping("/send")
+    public ResponseEntity<Message> sendMessage(@RequestParam String content, @RequestParam Long userId ){
+       UserResponse userResponse = userService.getById(userId);
+       Message savedMessage = messageService.sendMessage(content, userResponse);
+       return ResponseEntity.ok(savedMessage);
+   }
 
+   @GetMapping("/all")
+    public ResponseEntity<List<Message>> getAllMessages(){
+       List<Message> allMessages = messageService.getAllMessages();
+       return ResponseEntity.ok(allMessages);
+   }
 
-    public Message handleMessage(Message message) throws Exception {
-        MessageServiceImp.saveMessage(message);
+   @PostMapping("/save")
+    public ResponseEntity<Void> saveMessage(@RequestBody Message message){
+       messageService.saveMessage(message);
+       return ResponseEntity.ok().build();
+   }
 
-        return message;
-
-    }
 }
+
+
+
+
+
+
+
--- Back/src/main/java/com/s13nocoutry/LearnConnect/service/abstraction/MessageService.java
+++ Back/src/main/java/com/s13nocoutry/LearnConnect/service/abstraction/MessageService.java
@@ -1,14 +1,15 @@
 package com.s13nocoutry.LearnConnect.service.abstraction;
 
 import com.s13nocoutry.LearnConnect.models.Message.Message;
-import com.s13nocoutry.LearnConnect.models.user.User;
+import com.s13nocoutry.LearnConnect.models.user.UserResponse;
 
 import java.util.List;
 
 public interface MessageService {
-    Message sendMessage(String content, User user);
+    Message sendMessage(String content, UserResponse user);
 
     List<Message> getAllMessages();
 
 
+    void saveMessage(Message message);
 }
